d1444f9b57f63e8b488008b0409ebbff6ace2333
Vinay Kushwah <Vinaykushwah805@gmail.com>
2025-07-30 19:19:18 +0530

    Bug Fix

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,6 +1,7 @@
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('../utils/cloudinary'); // Make sure this path is correct
+const cloudinary = require('../cloudinaryConfig');
+
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
