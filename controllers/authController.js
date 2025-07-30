da36eae66929985eac2b6b991f7b73fdcddb1f30
Vinay Kushwah <Vinaykushwah805@gmail.com>
2025-07-30 16:02:11 +0530

    Fix image URL

--- controllers/authController.js
+++ controllers/authController.js
@@ -72,13 +72,18 @@ exports.getUserInfo = async (req,res)=> {
         const user = await User.findById(req.user.id).select("-password");
 
         if (!user) {
-            return res.status(404).json({ message: "User not found"});
+            return res.status(404).json({ message: "User not found"}); 
         }
 
-        res.status(200).json(user);
+        const BASE_URL = process.env.BASE_URL || "http://localhost:8000";
+
+        const updatedUser = {
+            ...user._doc,
+            profileImageUrl: user.profileImageUrl,
+        };
+
+        res.status(200).json(updatedUser);
     } catch (err) {
-        res 
-         .status(500)
-         .json({ message: "Error registering user" , error:err.message});
+        res.status(500).json({ message: "Error getting user", error: err.message });
     }
 };
